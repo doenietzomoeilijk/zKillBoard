dd7d1b74273c0a7dde9d5c76836f0d669b4e8e28
Clinton V. Weiss <cvweiss@gmail.com>
2011-07-29 11:49:42 -0400

    Removed log message.

--- util/price.php
+++ util/price.php
@@ -3,5 +3,4 @@
 function clearExpiredPrices() {
     global $dbPrefix;
     Db::execute("delete from {$dbPrefix}prices where expires < unix_timestamp()");
-    Log::irc("Expired prices test complete");
 }
